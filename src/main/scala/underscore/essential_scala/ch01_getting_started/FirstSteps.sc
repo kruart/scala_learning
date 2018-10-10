b2c118df4ddda0df9bc3a564d56daa11815a0356
we0z <weoz@ukr.net>
2018-10-10 19:43:19 +0300

    underscore_essential_scala: 1.1 getting_started

---
+++ src/main/scala/underscore/essential_scala/ch01_getting_started/FirstSteps.sc
@@ -0,0 +1,5 @@
+println("Welcome to the Scala worksheet")
+
+1 + 1
+if(20 > 10) "left" else "right"
+println("The ultimate answer is " + 42)
---
+++ src/main/scala/underscore/essential_scala/ch01_getting_started/HelloWorld.scala
@@ -0,0 +1,7 @@
+package underscore.essential_scala.ch01_getting_started
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
