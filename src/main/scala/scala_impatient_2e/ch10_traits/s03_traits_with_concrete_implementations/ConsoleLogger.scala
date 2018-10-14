88db1123bc8f430f1f12d8dba35beae04bef58e4
we0z <weoz@ukr.net>
2018-10-14 21:47:29 +0300

    scala_impatient: 10.3 Traits with Concrete Implementations

---
+++ src/main/scala/scala_impatient_2e/ch10_traits/s03_traits_with_concrete_implementations/ConsoleLogger.scala
@@ -0,0 +1,17 @@
+package scala_impatient_2e.ch10_traits.s03_traits_with_concrete_implementations
+
+trait ConsoleLogger {
+  def log(msg: String) = println(msg)
+}
+class Account {
+  var balance = 1000
+  def currentBalance = balance
+  def deposit(amount: Double) = { balance += amount; }
+  def withdraw(amount: Double) = { balance -= amount; }
+}
+class SavingsAccount extends Account with ConsoleLogger {
+  override def withdraw(amount: Double) {
+    if (amount > balance) log("Insufficient funds")
+    else balance -= amount
+  }
+}
