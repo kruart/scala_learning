728fefb7861ce0348bbe3682b85f26956950e607
we0z <weoz@ukr.net>
2018-11-25 12:14:41 +0200

    devInsideYou_YFPL: v07_Part_7.2_recursion

--- src/main/scala/devInsideYou/YFPL/v07_recursion/Main.scala
+++ src/main/scala/devInsideYou/YFPL/v07_recursion/Main.scala
@@ -53,7 +53,38 @@ object Main {
         loop
       }
       println(method3)
+      println("-" * 20 + "factorial" + "-" * 20)
+
+      def factorial(n: Int): Int = {
+        @tailrec
+        def loop(x: Int, acc: Int): Int = {
+          if(x == 0) acc
+          else loop(x - 1, acc * x)
+        }
+        loop(n, 1)
+      }
+
+      println(s"0! ${factorial(0)}")
+      println(s"1! ${factorial(1)}")
+      println(s"2! ${factorial(2)}")
+      println(s"3! ${factorial(3)}")
+      println(s"4! ${factorial(4)}")
+      println(s"5! ${factorial(5)}")
+      println("-" * 20 + "fibonacci" + "-" * 20)
+
+      def fib(n: Int): Int = {
+        @tailrec
+        def fib_tail(x: Int, current: Int, next: Int): Int = {
+          if(x == 0) current
+          else fib_tail(x - 1, next, current + next)
+        }
+        fib_tail(n, 0, 1)
+      }
+
+      println(s"fib(5) ${fib(50)}")
     }
+
+
     println("*" * 50)
   }
 }
